d2353f2547ab0de218e173fb914bab64cf997ee1
Valentine Lav <valentinelav98@gmail.com>
2024-05-03 11:06:41 +0200

    :card_file_box: Fix SQL file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,45 +1,52 @@
-CREATE TABLE Persons (
-    PersonID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    Email varchar(255) NOT NULL,
-    Password varchar(255) NOT NULL
+DROP TABLE Transactions CASCADE;
+DROP TABLE BankAccounts CASCADE;
+DROP TABLE Beneficiary CASCADE;
+DROP TABLE Users CASCADE;
+CREATE TABLE users (
+    userID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 );
 
-CREATE TABLE BankAccounts (
-    BankAccountID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE bankAccounts (
+    bankAccountID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL,
     RIB varchar(255),
-    PersonID int,
-    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+    userID int NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
-CREATE TABLE Transactions (
-    TransactionID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    SenderID INT NOT NULL ,
-    FOREIGN KEY (SenderID) REFERENCES Persons(PersonID),
-    Recipient varchar(255) NOT NULL,
-    Description varchar(255) NOT NULL,
-    Amount int NOT NULL,
-    BankAccountID INT NOT NULL,
-    FOREIGN KEY (BankAccountID) REFERENCES BankAccounts(BankAccountID)
+CREATE TABLE transactions (
+    transactionID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    senderID INT NOT NULL ,
+    FOREIGN KEY (senderID) REFERENCES users(userID),
+    recipient varchar(255) NOT NULL,
+    FOREIGN KEY (recipient) REFERENCES users(email),
+    description varchar(255) NOT NULL,
+    amount int NOT NULL,
+    bankAccountID INT NOT NULL,
+    FOREIGN KEY (bankAccountID) REFERENCES bankAccounts(bankAccountID)
 );
 
-CREATE TABLE Friends (
-    Person1ID INT NOT NULL,
-    Person2ID INT NOT NULL,
-    PRIMARY KEY (Person1ID, Person2ID),
-    FOREIGN KEY (Person1ID) REFERENCES Persons(PersonID),
-    FOREIGN KEY (Person2ID) REFERENCES Persons(PersonID)
+CREATE TABLE beneficiary (
+    sender INT NOT NULL,
+    recipient INT NOT NULL,
+    PRIMARY KEY (sender, recipient),
+    FOREIGN KEY (sender) REFERENCES users(userID),
+    FOREIGN KEY (recipient) REFERENCES users(userID)
 );
 
-INSERT INTO Persons (Email, Password)
-VALUES ('user1@example.com', 'password1'),
-    ('user2@example.com', 'password2');
+INSERT INTO users (firstName, email, password)
+VALUES ('User1','user1@example.com', 'password1'),
+    ('User2','user2@example.com', 'password2');
 
-INSERT INTO BankAccounts (RIB, PersonID)
-VALUES ('FR1234567890123456789012345', 1),
-    ('FR9876543210987654321012345', 2);
+INSERT INTO bankAccounts (email, RIB, userID)
+VALUES ('user1@example.com', 'FR1234567890123456789012345', 1),
+    ('user2@example.com', 'FR9876543210987654321012345', 2);
 
-INSERT INTO Friends (Person1ID, Person2ID)
+INSERT INTO beneficiary (sender, recipient)
 VALUES (1, 2), (2, 1);
 
-INSERT INTO Transactions (TransactionID, SenderID, Recipient, amount, description, BankAccountID)
-VALUES (1, 2, 'user2@example.com', 100.00, 'Payment for Book', 2);
+INSERT INTO transactions (transactionID, senderID, recipient, description, amount, bankAccountID)
+VALUES (1, 2, 'user2@example.com', 'Payment for Book', 100.00, 2);
