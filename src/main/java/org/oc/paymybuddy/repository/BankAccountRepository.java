bf1f7888b018d2efd52c6395522f8f8ea1601db0
Valentine Lav <valentinelav98@gmail.com>
2024-05-11 18:54:41 +0200

    :fire: Delete unnecessary lines

--- src/main/java/org/oc/paymybuddy/repository/BankAccountRepository.java
+++ src/main/java/org/oc/paymybuddy/repository/BankAccountRepository.java
@@ -2,10 +2,8 @@ package org.oc.paymybuddy.repository;
 
 import org.oc.paymybuddy.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableJpaRepositories
 public interface BankAccountRepository extends JpaRepository<BankAccount, Integer> {
 }
