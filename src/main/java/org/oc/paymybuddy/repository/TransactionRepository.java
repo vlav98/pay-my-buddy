e8dcfc4dcf3c7e90afe9c03eabe3d8879ba70ec7
Valentine Lav <valentinelav98@gmail.com>
2024-05-11 18:54:41 +0200

    :fire: Delete unnecessary lines

--- src/main/java/org/oc/paymybuddy/repository/TransactionRepository.java
+++ src/main/java/org/oc/paymybuddy/repository/TransactionRepository.java
@@ -2,11 +2,9 @@ package org.oc.paymybuddy.repository;
 
 import org.oc.paymybuddy.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableJpaRepositories
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     boolean existsByTransactionID(Integer ID);
 
