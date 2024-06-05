c070085d2a9952e783703884b7b1abe82cc07847
Valentine Lav <valentinelav98@gmail.com>
2024-06-05 12:40:12 +0200

    :sparkles: Profile Page Added

--- src/main/java/org/oc/paymybuddy/controller/FragmentsController.java
+++ src/main/java/org/oc/paymybuddy/controller/FragmentsController.java
@@ -1,13 +1,24 @@
 package org.oc.paymybuddy.controller;
 
+import org.oc.paymybuddy.model.User;
+import org.oc.paymybuddy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FragmentsController {
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/index")
-    public String getIndex() {
-        return "index";
+    @GetMapping("/profile")
+    public String getProfile(Model model) throws Exception {
+        User connectedUser = userService.getAuthenticatedUser();
+
+        model.addAttribute("user", connectedUser);
+        model.addAttribute("page", "profile");
+
+        return "profile";
     }
 }
